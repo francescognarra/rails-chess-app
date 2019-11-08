dc4f1072cb18c3ce7911de585dbfa60597e1f156
Andy Strube <andystrube64@gmail.com>
2019-11-07 20:13:37 -0800

    Busted code

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,43 +1,44 @@
 class GamesController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [ :update ]
-  before_action :authenticate_user!
-
-  def index
-    @game = Game.all
+    skip_before_action :verify_authenticity_token, only: [ :update ]
+    before_action :authenticate_user!
+  
+    def index
+      @game = Game.all
+    end
+  
+    def new
+      @game = Game.new
+    end
+  
+    def show
+      game = Game.find(params[:id])
+      render json: game.as_json()
+    end
+  
+    def create
+      @game = current_user.games.create(game_params)
+      redirect_to edit_game_path(@game)
+    end
+  
+    def edit
+      @game = Game.find(params[:id])
+    end
+  
+    def update
+      game = Game.find(params[:id])
+      game.update_attribute(:board, board_param["board"])
+      render json: game.as_json()
+    end
+  
+    private
+  
+    def board_param
+      params.require(:game)
+    end
+  
+    def game_params
+      params.require(:game).permit(:name)
+    end
+  
   end
-
-  def new
-    @game = Game.new
-  end
-
-  def show
-    game = Game.find(params[:id])
-    render json: game.as_json()
-  end
-
-  def create
-    @game = current_user.games.create(game_params)
-    redirect_to edit_game_path(@game)
-  end
-
-  def edit
-    @game = Game.find(params[:id])
-  end
-
-  def update
-    game = Game.find(params[:id])
-    game.update_attribute(:board, board_param["board"])
-    render json: game.as_json()
-  end
-
-  private
-
-  def board_param
-    params.require(:game)
-  end
-
-  def game_params
-    params.require(:game).permit(:name)
-  end
-
-end
+  
