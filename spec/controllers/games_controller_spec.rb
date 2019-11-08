226000d458726a3ada838319989a9890154d8f37
Andy Strube <andystrube64@gmail.com>
2019-11-08 15:26:07 -0800

    Added a test

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
 
-  describe "grams#index action" do
-    
+  describe "games#index action" do
     it "should successfully show the page" do
       user = User.create(
         email:                 'fakeuser@gmail.com',
@@ -15,6 +14,10 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
 
+    it "should require users to be logged in" do
+      get :index
+      expect(response).to redirect_to new_user_session_path
+    end
   end
 
 end
