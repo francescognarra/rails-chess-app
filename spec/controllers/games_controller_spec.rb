9842613d98184aed1364f27176fe27b0c78b9b54
Andy Strube <andystrube64@gmail.com>
2019-11-07 21:11:45 -0800

    Configured devise for testing functionality

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe GamesController, type: :controller do
   describe "grams#index action" do
     
     it "should successfully show the page" do
+      user = User.create(
+        email:                 'fakeuser@gmail.com',
+        password:              'secretPassword',
+        password_confirmation: 'secretPassword'
+      )
+      sign_in user
       get :index
       expect(response).to have_http_status(:success)
     end
