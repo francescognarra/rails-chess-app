18cb0d4e2725c3889d7df8249ba049c44249e607
Andy Strube <andystrube64@gmail.com>
2019-11-09 02:16:11 -0800

    Added some tests for the update action

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -2,6 +2,29 @@ require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
 
+  describe "games#new action" do
+    it "should successfully show the page" do
+      user = FactoryBot.create(:user)
+      sign_in user
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should require a user be logged in" do
+      get :new
+      expect(response).to redirect_to new_user_session_path
+    end
+  end
+
+  describe "games#create action" do
+    it "should be able to create a new game" do
+      user = FactoryBot.create(:user)
+      sign_in user
+      post :create, params: { game: { name: 'name' } }
+      expect(response).to have_http_status(:found)
+    end
+  end
+
   describe "games#index action" do
     it "should successfully show the page" do
       user = FactoryBot.create(:user)
@@ -31,26 +54,42 @@ RSpec.describe GamesController, type: :controller do
     end
   end
 
-  describe "games#create action" do
-    it "should be able to create a new game" do
-      user = FactoryBot.create(:user)
-      sign_in user
-      post :create, params: { game: { name: 'name' } }
-      expect(response).to have_http_status(:found)
-    end
-  end
-
-  describe "games#new action" do
-    it "should successfully show the page" do
-      user = FactoryBot.create(:user)
-      sign_in user
-      get :new
+  describe "games#update action" do
+    it "should allow boards to be updated" do
+      game = FactoryBot.create(:game)
+      sign_in game.user
+      put :update, params: {id: game.id, game: { board: [
+      ["♖", "♘", "♗", "♔", "♕", "♗",  "♘", "♖"],
+        ['', "♙", "♙", "♙", "♙", "♙", "♙", "♙"],
+        ["♙",'','','','','','',''],
+        ['','','','','','','',''],
+        ['','','','','','','',''],
+        ['','','','','','','',''],
+        ["♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"],
+        ["♜", "♞", "♝", "♚", "♛", "♝", "♞", "♜"]
+      ]} }
       expect(response).to have_http_status(:success)
+      game.reload
+      expect(game.board[8][0]).to eq ''
+      expect(game.board[16][0]).to eq "♙"
     end
 
     it "should require a user be logged in" do
-      get :new
+      game = FactoryBot.create(:game)
+      put :update, params: {id: game.id, game: { board: [
+      ["♖", "♘", "♗", "♔", "♕", "♗",  "♘", "♖"],
+        ['', "♙", "♙", "♙", "♙", "♙", "♙", "♙"],
+        ["♙",'','','','','','',''],
+        ['','','','','','','',''],
+        ['','','','','','','',''],
+        ['','','','','','','',''],
+        ["♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"],
+        ["♜", "♞", "♝", "♚", "♛", "♝", "♞", "♜"]
+      ]} }
       expect(response).to redirect_to new_user_session_path
+      game.reload
+      expect(game.board[1][0]).to eq "♙"
+      expect(game.board[2][0]).to eq ''
     end
   end
 
