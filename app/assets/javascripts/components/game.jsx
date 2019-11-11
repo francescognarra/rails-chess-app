7e1452faaf1705b0de7a28b6fd924eddf2bc023e
Andy Strube <andystrube64@gmail.com>
2019-11-11 03:16:24 -0800

    Updated axios for production

--- app/assets/javascripts/components/game.jsx
+++ app/assets/javascripts/components/game.jsx
@@ -1224,19 +1224,19 @@ class Game extends React.Component {
   }
 
   deleteGame() {
-    // axios.delete(`https://chess-app-rails-andy-strube.herokuapp.com/games/` + this.props.id)
-    axios.delete(`http://localhost:3000/games/` + this.props.id)
+    axios.delete(`https://chess-app-rails-andy-strube.herokuapp.com/games/` + this.props.id)
+    //axios.delete(`http://localhost:3000/games/` + this.props.id)
   }
 
   updateBoard() {
-    //axios.patch('https://chess-app-rails-andy-strube.herokuapp.com/games/' + this.props.id, {board: this.state.board})
-    axios.patch('http://localhost:3000/games/' + this.props.id, {board: this.state.board})
+    axios.patch('https://chess-app-rails-andy-strube.herokuapp.com/games/' + this.props.id, {board: this.state.board})
+    //axios.patch('http://localhost:3000/games/' + this.props.id, {board: this.state.board})
     .catch((err) => console.log(err.response.data) );
   }
 
   requestBoardFromDataBase() {
-    //axios.get('https://chess-app-rails-andy-strube.herokuapp.com/games/' + this.props.id)
-    axios.get('http://localhost:3000/games/' + this.props.id)
+    axios.get('https://chess-app-rails-andy-strube.herokuapp.com/games/' + this.props.id)
+    //axios.get('http://localhost:3000/games/' + this.props.id)
     .then((res) =>
     this.setState({
       board: res.data.board
