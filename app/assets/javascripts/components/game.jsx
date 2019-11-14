e463b4ce43b75d9e8f811ee97f4d0b69586bfe0f
Andy Strube <andystrube64@gmail.com>
2019-11-13 18:07:58 -0800

    Minor adjustements for bug shooting

--- app/assets/javascripts/components/game.jsx
+++ app/assets/javascripts/components/game.jsx
@@ -1229,15 +1229,16 @@ class Game extends React.Component {
   }
 
   vetUpdates(res) {
+    console.log(res.data.updated_at);
     let newTime = Number(
-      res.data.updated_at[0] +
-      res.data.updated_at[1] +
-      res.data.updated_at[2] +
-      res.data.updated_at[3] +
-      res.data.updated_at[5] +
-      res.data.updated_at[6] +
-      res.data.updated_at[8] +
-      res.data.updated_at[9] +     
+      // res.data.updated_at[0] +
+      // res.data.updated_at[1] +
+      // res.data.updated_at[2] +
+      // res.data.updated_at[3] +
+      // res.data.updated_at[5] +
+      // res.data.updated_at[6] +
+      // res.data.updated_at[8] +
+      // res.data.updated_at[9] +     
       res.data.updated_at[11] +
       res.data.updated_at[12] +
       res.data.updated_at[14] +
@@ -1245,6 +1246,7 @@ class Game extends React.Component {
       res.data.updated_at[17] +
       res.data.updated_at[18]
     );
+    console.log(newTime);
     if(newTime > this.state.updateTime) {
       this.setState({
         board: res.data.board,
@@ -1264,7 +1266,7 @@ class Game extends React.Component {
   componentDidMount() {
     this.interval = setInterval(() => {
       this.requestBoardFromDataBase();
-    }, 1500);
+    }, 150);
   }
 
   componentWillMount() {
