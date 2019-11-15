73100302f037483a2be171814e38e27e3d90214f
Andy Strube <andystrube64@gmail.com>
2019-11-14 23:39:46 -0800

    configured axios for production

--- app/assets/javascripts/components/game.jsx
+++ app/assets/javascripts/components/game.jsx
@@ -1267,8 +1267,8 @@ class Game extends React.Component {
   }
 
   updateBoard() {
-    //axios.patch('https://chess-app-rails-andy-strube.herokuapp.com/games/' + this.props.id, {board: this.state.board})
-    axios.patch('http://localhost:3000/games/' + this.props.id, {board: this.state.board})
+    axios.patch('https://chess-app-rails-andy-strube.herokuapp.com/games/' + this.props.id, {board: this.state.board})
+    //axios.patch('http://localhost:3000/games/' + this.props.id, {board: this.state.board})
     .catch((err) => console.log(err.response.data) );
     console.log("A patch request was made");
   }
@@ -1299,8 +1299,8 @@ class Game extends React.Component {
   }
 
   requestBoardFromDataBase() {
-    //axios.get('https://chess-app-rails-andy-strube.herokuapp.com/games/' + this.props.id)
-    axios.get('http://localhost:3000/games/' + this.props.id)
+    axios.get('https://chess-app-rails-andy-strube.herokuapp.com/games/' + this.props.id)
+    //axios.get('http://localhost:3000/games/' + this.props.id)
     .then((res) =>
       this.vetUpdates(res)
     )
