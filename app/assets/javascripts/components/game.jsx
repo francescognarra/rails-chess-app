45ee21c0841053cb02e7b16c113c62bc99b5785e
Andy Strube <andystrube64@gmail.com>
2019-11-17 15:28:26 -0800

    Disabled turbolinks for the game component

--- app/assets/javascripts/components/game.jsx
+++ app/assets/javascripts/components/game.jsx
@@ -1187,7 +1187,7 @@ class Game extends React.Component {
   componentDidMount() {
     this.interval = setInterval(() => {
       this.requestBoardFromDataBase();
-    }, 150);
+    }, 1500);
   }
 
   componentWillMount() {
