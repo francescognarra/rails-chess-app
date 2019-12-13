80e594e38632db294a03ee44334f89419c5e9a71
Andy Strube <andystrube64@gmail.com>
2019-12-13 14:01:34 -0800

    Added turns to the database

--- db/migrate/20191025001914_create_games.rb
+++ db/migrate/20191025001914_create_games.rb
@@ -14,6 +14,7 @@ class CreateGames < ActiveRecord::Migration[5.2]
       t.string "player_1", default: ""
       t.string "player_2", default: ""
       t.integer "user_id"
+      t.boolean :black_teams_turn, default: false
       t.timestamps
     end
   end
