4a545a317d809ad249a422435d50f743c6e28fc9
Andy Strube <andystrube64@gmail.com>
2020-01-09 13:24:36 -0800

    Minor UI touch ups

--- app/assets/javascripts/components/winnerStatement.jsx
+++ app/assets/javascripts/components/winnerStatement.jsx
@@ -9,7 +9,7 @@ class WinnerStatement extends React.Component {
           {this.props.player_2} lost
           <br />
           <button className="winner-statement-button">
-            <h6><span><a href='https://chess-app-rails-andy-strube.herokuapp.com/index/' className="winner-statement-button-color">End Match and return to lobby</a></span></h6>
+            <h6><span><a href='https://chess-app-rails-andy-strube.herokuapp.com/index/' className="winner-statement-button-text">End Match and return to lobby</a></span></h6>
           </button>
         </div>
       );
@@ -22,7 +22,7 @@ class WinnerStatement extends React.Component {
           {this.props.player_1} lost
           <br />
           <button className="winner-statement-button">
-            <h6><span><a href='https://chess-app-rails-andy-strube.herokuapp.com/index/' className="winner-statement-button-color">End Match and return to lobby</a></span></h6>
+            <h6><span><a href='https://chess-app-rails-andy-strube.herokuapp.com/index/' className="winner-statement-button-text">End Match and return to lobby</a></span></h6>
           </button>
         </div>
       );
